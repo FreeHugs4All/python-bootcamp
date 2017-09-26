aa5e4651538914da1c27a811a4360c117fc6140e
FreeHugs4All <31968536+FreeHugs4All@users.noreply.github.com>
2017-09-26 13:43:09 -0400

    Create reverse_a_string.py

---
+++ capstone_projects/text/reverse_a_string.py
@@ -0,0 +1,7 @@
+# Reverse a String - 
+# Enter a string and the program will reverse it and print it out.
+
+# strings can be reversed with the use of a negative step
+
+str = input("Enter a string and I'll print out it out in reverse. ")
+print(str[::-1])
