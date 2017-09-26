0dc3c0bdbefb7500399525f3c052fa6c879c0966
FreeHugs4All <31968536+FreeHugs4All@users.noreply.github.com>
2017-09-26 14:25:57 -0400

    Create count_vowels.py

---
+++ capstone_projects/text/count_vowels.py
@@ -0,0 +1,15 @@
+# Count Vowels - 
+# Enter a string and the program counts the number of vowels in the text. 
+# For added complexity have it report a sum of each vowel found.
+
+
+list_vowels = ['a','e','i','o','u']
+stringInput = input("Enter a string and I'll compute the number of vowels in it. ")
+
+def print_vowel_counts(vowel, count):
+	print ("There are %s occurences of '%s' in the string \n" % (count, vowel))
+
+# str.count(char) returns the number of times char shows up in str
+print("\nYour string is: %s" % (stringInput)) 
+for item in list_vowels:
+	print_vowel_counts(item, stringInput.count(item))
