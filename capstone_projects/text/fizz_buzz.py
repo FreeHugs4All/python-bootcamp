b645a70e3c30f20bfff440442aac7831908276d9
FreeHugs4All <31968536+FreeHugs4All@users.noreply.github.com>
2017-09-26 13:38:04 -0400

    Create fizz_buzz.py

---
+++ capstone_projects/text/fizz_buzz.py
@@ -0,0 +1,15 @@
+# Fizz Buzz -
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number.
+# For the multiples of five print “Buzz”. 
+# For numbers which are multiples of both three and five print “FizzBuzz”.
+
+for i in range(100+1):
+	if i % 3 == 0 and i % 5 == 0:
+		print ("FizzBuzz")
+	elif i % 3 == 0:
+		print("Fizz")
+	elif i % 5 == 0:
+		print("Buzz")
+	else:
+		print (i)
